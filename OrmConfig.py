2ae8ee08ab43492307c3fd10ac1015e3db458736
totallynotfe <felipe_ramalheira@outlook.com>
2023-03-24 00:46:56 -0300

    Configuração do SqlAlchemy e implementação do padrão MVC no projeto

--- OrmConfig.py
+++ OrmConfig.py
@@ -1,9 +1,8 @@
-from typing import Optional
+import datetime
 from sqlalchemy import ForeignKey, MetaData, String, DateTime, create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
-from sqlalchemy.orm import relationship
 
 
 metadata_obj = MetaData()
@@ -15,30 +14,53 @@ class Base(DeclarativeBase):
 
 class User(Base):
     __tablename__ = "user_account"
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(30))
-    email: Mapped[Optional[str]]
+    cpf: Mapped[str] = mapped_column(String)
+    address: Mapped[str] = mapped_column(String)
+    birth: Mapped[DateTime] = mapped_column(DateTime)
+    first: Mapped[DateTime] = mapped_column(DateTime)
+    last: Mapped[DateTime] = mapped_column(DateTime)
 
-    def __init__(self, name, email):
-        self.name(name)
-        self.email(email)
+    def __init__(self, name: str, cpf: str, address: str, birth: datetime, first: datetime, last: datetime):
+        self.name = name
+        self.cpf = cpf
+        self.address = address
+        self.birth = birth
+        self.first = first
+        self.last = last
 
     def __repr__(self) -> str:
-        return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
+        return f"User(id={self.id!r}, name={self.name!r})"
 
 
-class Exam(Base):
-    __tablename__ = "user_exam"
-    id: Mapped[int] = mapped_column(primary_key=True)
+class Exams(Base):
+    __tablename__ = "user_exams"
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    value: Mapped[float] = mapped_column(float)
+    procedure_id: Mapped[int] = mapped_column(ForeignKey("procedures.id"))
+    sessions: Mapped[int] = mapped_column(int)
     date: Mapped[DateTime] = mapped_column(DateTime)
     user_id: Mapped[int] = mapped_column(ForeignKey("user_account.id"))
-    user: Mapped["User"] = relationship(back_populates="addresses")
-    # Trigliceres
-    # Colesterol
-    # Glicemia
+
+    def __init__(self, value: float, procedure_id: int, sessions: int, date: datetime, user_id: int):
+        self.value = value
+        self.procedure_id = procedure_id
+        self.sessions = sessions
+        self.date = date
+        self.user_id = user_id
 
     def __repr__(self) -> str:
-        return f"Address(id={self.id!r}, email_address={self.email_address!r})"
+        return f"Exam()"
+
+
+class Procedure(Base):
+    __tablename__ = "procedures"
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(40))
+
+    def __init__(self, name):
+        self.name = name
 
 
 engine = create_engine("sqlite://", echo=True)
