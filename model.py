eed5b874379de65f05571a4a64953ca104a5b42b
totallynotfe <felipe_ramalheira@outlook.com>
2023-04-11 16:40:29 -0300

    conclusão das funções de CRUD para todas as tabelas

--- model.py
+++ model.py
@@ -25,3 +25,75 @@ def delete_user(id_in: int):
 def read_user(id_in: int):
     user = OrmConfig.session.query(OrmConfig.User).filter(OrmConfig.User.id == id_in)
     return user
+
+
+def get_all_users():
+    return OrmConfig.session.query(OrmConfig.User).all()
+
+
+def create_doctor(crm: int, name: str, address: str, birth: datetime, cpf: str, first: datetime, last: datetime):
+    new_doctor = OrmConfig.Doctor(crm, name, address, birth, cpf, first, last)
+    OrmConfig.session.add(new_doctor)
+    OrmConfig.session.commit()
+
+
+def update_doctor(data):
+    doctor = OrmConfig.session.query(OrmConfig.Doctor).filter(OrmConfig.Doctor.id == data.id)
+    for k, v in data.items():
+        doctor[k] = v
+    OrmConfig.session.commit()
+
+
+def delete_doctor(id_in: int):
+    doctor = OrmConfig.session.query(OrmConfig.Doctor).filter(OrmConfig.Doctor.id == id_in)
+    OrmConfig.session.delete(doctor)
+    OrmConfig.session.commit()
+
+
+def read_doctor(id_in: int):
+    doctor = OrmConfig.session.query(OrmConfig.Doctor).filter(OrmConfig.Doctor.id == id_in)
+    return doctor
+
+
+def get_all_doctors():
+    return OrmConfig.session.query(OrmConfig.Doctor).all()
+
+
+def create_procedure(name_in: str):
+    new_procedure = OrmConfig.Procedure(name_in)
+    OrmConfig.session.add(new_procedure)
+
+
+def delete_procedure(id_in: int):
+    procedure = OrmConfig.session.query(OrmConfig.Procedure).filter(OrmConfig.Procedure.id == id_in)
+    OrmConfig.session.delete(procedure)
+    OrmConfig.session.commit()
+
+
+def get_all_procedures():
+    return OrmConfig.session.query(OrmConfig.Procedure).all()
+
+
+def create_exam(value_in: float, procedure_id_in: int, sessions_in: int, date_in: datetime, user_id_in: int,
+                doctor_id_in: int):
+    new_exam = OrmConfig.Exams(value_in, procedure_id_in, sessions_in, date_in, user_id_in, doctor_id_in)
+    OrmConfig.session.add(new_exam)
+    OrmConfig.session.commit()
+
+
+def update_exam(data):
+    exam = OrmConfig.session.query(OrmConfig.Exams).filter(OrmConfig.Exams.id == data.id)
+    for k, v in data.items():
+        exam[k] = v
+    OrmConfig.session.commit()
+
+
+def delete_exam(id_in: int):
+    exam = OrmConfig.session.query(OrmConfig.Exams).filter(OrmConfig.Exams.id == id_in)
+    OrmConfig.session.delete(exam)
+    OrmConfig.session.commit()
+
+
+def read_exam(id_in: int):
+    exam = OrmConfig.session.query(OrmConfig.Exams).filter(OrmConfig.Exams.id == id_in)
+    return exam
